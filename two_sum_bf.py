9583e03ce02b55fe1b24ad40c6bce64a67b4d82b
Bhumijaa Balaji <bhumijaabalaji@Bhumijaas-MacBook-Air.local>
2025-03-05 17:12:36 -0500

    two sum brute force solution in python

--- two_sum_bf.py
+++ two_sum_bf.py
@@ -2,10 +2,9 @@
 # Brute Force Solution
 class Solution(object):
     def twoSum(self, nums, target):
-        map = {}
-        for i, num in enumerate(nums):
-            if num in map:
-                return [map[num], i]
-            map[target - num] = i
-
-                    
+        for i in range(len(nums)):
+            number_to_find = target - nums[i]
+            for j in range(i + 1, len(nums)):
+                if nums[j] == number_to_find:
+                    return [i, j]
+                        
